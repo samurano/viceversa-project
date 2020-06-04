e5295eaf2dca6c435a80cebb30578c9e6ca439c1
Vitaliy Semyanov <v.semyanov@gmail.com>
2020-06-04 20:30:52 +0700

    Final version

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.home),
+    path('reversed/', views.reverse, name='reverse'),
 ]
